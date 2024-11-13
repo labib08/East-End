e1042c328f80b2961617b12fd3dc35838c06a111
labib08 <iklabib49@gmail.com>
2024-11-14 07:48:46 +1100

    Added the home page and the welcome page

--- src/Components/WelcomePage/WelcomePage.tsx
+++ src/Components/WelcomePage/WelcomePage.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react';
+import intro_photo_2 from '../../Assets/intro_photo_2.webp';
+import intro_photo_3 from '../../Assets/intro_photo_3.avif';
+import intro_photo_4 from '../../Assets/intro_photo_4.webp';
 
 const WelcomePage = () => {
+  const images = [intro_photo_2, intro_photo_3, intro_photo_4];
+  const [currImage, setCurrImage] = useState<number>(0);
+  const [fade, setFade] = useState<boolean>(true);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFade(false);
+      setTimeout(() => {
+        setCurrImage((prevImage) => (prevImage + 1) % images.length);
+        setFade(true);
+      }, 2000);
+    }, 6000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const backgroundStyle: React.CSSProperties = {
+    backgroundImage: `url(${images[currImage]})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    borderRadius: '35px',
+    height: '34vw',
+    margin: '30px auto',
+    marginLeft: '50px',
+    marginRight: '50px',
+    position: 'relative',
+    opacity: fade ? 1 : 0,
+    transition: 'opacity 3s ease-in-out',
+  };
   return (
-    <div>WelcomePage</div>
+    <div
+      className="welcome-page"
+      style={backgroundStyle}
+    >
+      <div className="absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw]">
+        <h2 className='font-medium text-white text-[max(4.5vw,22px)]'>Order your favorite coffee here</h2>
+        <p className='text-white text-[1vw]'>Choose from a diverse range of coffees and desserts featuring the World's finest coffee beans</p>
+        <button className='border-none text-black p-[1vw_2.3vw] bg-white text-[max(1vw,13px)] rounded-full'>View Coffee</button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Data/Location.ts
@@ -0,0 +1,58 @@
+interface Location {
+    branch_name: string;
+    location: string;
+    open_hours: string;
+}
+
+export const locationData: Location[] = [
+    {
+        branch_name: "East End At Suburb1",
+        location: "Parkinson St, Suburb1, State1",
+        open_hours: "Mon-Fri: 9:00am-5:00pm",
+    },
+    {
+        branch_name: "East End At Suburb2",
+        location: "Random St, Suburb2, State1",
+        open_hours: "Mon-Fri: 9:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb3",
+        location: "Newcastle St, Suburb3, State2",
+        open_hours: "Mon-Fri: 7:00am-8:00pm"
+    },
+    {
+        branch_name: "East End At Suburb4",
+        location: "Hawthorn St, Suburb4, State2",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb5",
+        location: "Parkinson St, Suburb5, State2",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb6",
+        location: "Elizabeth St, Suburb6, State3",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb7",
+        location: "King St, Suburb7, State3",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb8",
+        location: "Queen St, Suburb8, State4",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb9",
+        location: "Jack St, Suburb9, State5",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+    {
+        branch_name: "East End At Suburb9",
+        location: "Ace St, Suburb9, State5",
+        open_hours: "Mon-Fri: 7:00am-11:00pm"
+    },
+];
