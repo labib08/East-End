925b4cb120748d7f8cb4e90f4b559bd8c42fceff
labib08 <iklabib49@gmail.com>
2024-11-18 23:50:11 +1100

    Changed the name of the product page to order page and updated its route

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Navbar } from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import HomeCategory from './Pages/HomeCategory/HomeCategory';
 import Login from './Pages/Login/Login';
-import Product from './Pages/Product/Product';
+import Order from './Pages/Order/Order';
 import SignUp from './Pages/SignUp/SignUp';
 function App() {
   return (
@@ -18,9 +18,7 @@ function App() {
           <Route path = "/coffee" element = {<HomeCategory category = "coffee"/>} />
           <Route path = "/dessert" element = {<HomeCategory category = "dessert"/>} />
           <Route path = "/merch" element = {<HomeCategory category = "merch"/>} />
-          <Route path = "/product" element = {<Product/>}>
-            <Route path = ":productID" element = {<Product/>}/>
-          </Route>
+          <Route path = "/order" element = {<Order/>}/>
           <Route path = "/cart" element = {<HomeCategory category = "cart"/>} />
           <Route path = "/login" element = {<Login/>}/>
           <Route path = "/signup" element = {<SignUp/>}/>
