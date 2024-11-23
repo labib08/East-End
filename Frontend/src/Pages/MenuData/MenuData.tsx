2caa7e1b21a20586e275cb3c9023a1b799d5ddbc
labib08 <iklabib49@gmail.com>
2024-11-23 19:21:01 +1100

    Added the function for displaying the menu in the admin panel by connecting to the backend

--- Frontend/src/Pages/MenuData/MenuData.tsx
+++ Frontend/src/Pages/MenuData/MenuData.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const MenuData: React.FC = () => {
+  const url = "http://localhost:5000";
+  const [menuList, setMenuList] = useState([]);
+  const getMenuItem = async () => {
+    const response = await axios.get(`${url}/api/item/list`);
+    if (response.data.success) {
+      setMenuList(response.data.data);
+    }
+    else {
+      toast.error("Error");
+    }
+  }
   return (
     <div>MenuData</div>
   )
