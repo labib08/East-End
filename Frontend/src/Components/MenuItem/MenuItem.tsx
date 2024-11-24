ed58c88484243bd4c86a04bdc504e9a80bb0db94
labib08 <iklabib49@gmail.com>
2024-11-25 03:28:23 +1100

    Fixed the error where the menu was not displaying images

--- Frontend/src/Components/MenuItem/MenuItem.tsx
+++ Frontend/src/Components/MenuItem/MenuItem.tsx
@@ -17,11 +17,11 @@ interface Props{
     setCartItems: React.Dispatch<React.SetStateAction<CartItems>>;
 }
 const MenuItem = ({id, name, image, price, description, addToCart, removeFromCart, cartItems, setCartItems}: Props) => {
-
+  const url = "http://localhost:5000";
   return (
     <div className=" w-full mx-auto rounded-[15px] shadow-[0px_0px_10px_#00000015] min-h-[480px] flex flex-col transition-[all_0.4s] fade-in">
         <div className="w-full rounded-t-[15px]">
-            <img className="w-full rounded-t-[15px]  h-[170px]" src = {image} alt = ""/>
+            <img className="w-full rounded-t-[15px]  h-[170px]" src = {`${url}/images/${image}`} alt = ""/>
         </div>
         <div className="p-[20px] flex flex-col flex-grow justify-between">
         <div className="mb-[10px]">
