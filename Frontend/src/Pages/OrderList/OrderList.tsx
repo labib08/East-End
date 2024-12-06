c827159b079f6853d060acbecdf4f400755b5cdd
labib08 <iklabib49@gmail.com>
2024-12-06 23:01:58 +1100

    My orderlist in admin panel only shows the items that have been ordered

--- Frontend/src/Pages/OrderList/OrderList.tsx
+++ Frontend/src/Pages/OrderList/OrderList.tsx
@@ -65,45 +65,52 @@ const OrderList: React.FC = () => {
   }, [])
   return (
     <div>
-      <h3 className='font-[600]'>Order Page</h3>
+      <h3 className="font-[600]">Order Page</h3>
       <div>
-        {orders.map((order, index)=>(
-          <div key = {index}className='grid grid-cols-[0.5fr_2fr_1fr_1fr_1fr] items-start gap-[30px] border border-[rgb(92,22,22)] p-[20px] my-[30px] text-[14px] text-[#505050] md-lg:text-[12px] md-lg:grid-cols-[0.5fr_2fr_1fr] md-lg:py-[15px] md-lg:px-[8px]'>
-            <img className='w-[500px] md-lg:w-[40px]' src = {orderPhoto} alt = "" />
-            <div>
-              <p className='font-[600]'>
-                {order.items.map((item, index)=>{
-                  if (index === order.items.length -1) {
-                    return item.name + " x " + item.quantity
-                  }
-                  else {
-                    return item.name + " x " + item.quantity + ", "
-                  }
-                })}
-              </p>
-              {order.address ? (
-                <p className="mt-[30px] mb-[5px] font-[600]">
-                  {`${order.address.firstName} ${order.address.lastName}`}
+        {orders
+          .filter(order => order.address)
+          .map((order, index) => (
+            <div
+              key={index}
+              className="grid grid-cols-[0.5fr_2fr_1fr_1fr_1fr] items-start gap-[30px] border border-[rgb(92,22,22)] p-[20px] my-[30px] text-[14px] text-[#505050] md-lg:text-[12px] md-lg:grid-cols-[0.5fr_2fr_1fr] md-lg:py-[15px] md-lg:px-[8px]"
+            >
+              <img className="w-[500px] md-lg:w-[40px]" src={orderPhoto} alt="" />
+              <div>
+                <p className="font-[600]">
+                  {order.items.map((item, i) =>
+                    i === order.items.length - 1
+                      ? item.name + " x " + item.quantity
+                      : item.name + " x " + item.quantity + ", "
+                  )}
                 </p>
-              ) : null}
-              <div className='mb-[10px]'>
-                <p> {order.address?.street + ", "} </p>
-                <p> {order.address?.city + ", " + order.address?.state +", " + order.address?.country+ ", " + order.address?.postcode} </p>
+                <p className="mt-[30px] mb-[5px] font-[600]">
+                  {`${order.address?.firstName} ${order.address?.lastName}`}
+                </p>
+                <div className="mb-[10px]">
+                  <p>{order.address?.street + ", "}</p>
+                  <p>
+                    {`${order.address?.city}, ${order.address?.state}, ${order.address?.country}, ${order.address?.postcode}`}
+                  </p>
+                </div>
+                <p>{order.address?.phone}</p>
               </div>
-              <p> {order.address?.phone} </p>
+              <p>Items: {order.items.length}</p>
+              <p>${order.amount ? order.amount.toFixed(1) : "0.0"}</p>
+              <select
+                onChange={(e) => statusHandler(e, order._id)}
+                value={order.status}
+                className="bg-[#ffe8e4] border border-[rgb(92,22,22)] w-[max(16.2vw,120px)] p-[10px] text-[14px] outline-none md-lg:p-[5px] md-lg:text-[12px]"
+              >
+                <option value="Restaurant is making the item">Restaurant is making the item</option>
+                <option value="Rider has picked up the order">Rider has picked up the order</option>
+                <option value="Delivered">Delivered</option>
+              </select>
             </div>
-            <p> Items: {order.items.length} </p>
-            <p>${order.amount.toFixed(1)}</p>
-            <select onChange={(e) => statusHandler(e, order._id)} value = {order.status} className='bg-[#ffe8e4] border border-[rgb(92,22,22)] w-[max(16.2vw,120px)] p-[10px] text-[14px] outline-none md-lg:p-[5px] md-lg:text-[12px]'>
-              <option value="Restaurant is making the item">Restaurant is making the item</option>
-              <option value="Rider has picked up the order">Rider has picked up the order</option>
-              <option value="Delivered">Delivered</option>
-            </select>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
-  )
-}
+
+      )
+    }
 
 export default OrderList
